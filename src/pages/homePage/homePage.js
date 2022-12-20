73d023f2a9c45faeb31a873cffaa58616d61f7ef
Maks Skomorokha <91227382+connars@users.noreply.github.com>
2022-12-21 01:54:40 +0200

    add

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -8,6 +8,10 @@ import item3 from './../../assets/images/3.png'
 import CardAva from './../../assets/images/icon1.svg'
 import CardAva2 from './../../assets/images/icon2.svg'
 import CardAva3 from './../../assets/images/icon3.svg'
+import i1 from './../../assets/images/i1.png'
+import i2 from './../../assets/images/i2.png'
+import i3 from './../../assets/images/i3.png'
+import i4 from './../../assets/images/i4.png'
 
 import HeroWidget from "../../components/heroWidget";
 
@@ -22,8 +26,9 @@ function HomePage() {
     }, []);
     let { t, i18n } = useTranslation();
     return (
+        
         <div className='home__wrapper'>
-            <div className='home__top-wrapper'>
+            {/* <div className='home__top-wrapper'>
                 <div className='home__top container'>
                     <h1 className='home__heading'>
                         {t("homePage.title")}
@@ -42,8 +47,70 @@ function HomePage() {
                     </p>
                     <div className='home__top-eagle'></div>
                 </div>
+            </div> */}
+        
+
+          <div className='giv'>
+
+            <div className='container z'>
+                <div className='giv__wrapper'>
+                    <div className='giv__left'>
+                        <h1> Глобальный новогодний розыгрыш среди <span className='yellow'>действующих</span> клиентов</h1>
+                        <div className='giv__left-bottom'>
+                            <h3 className='giv__text'>
+                                Присоеденяйся к компании HILLHOUSE сегодня и становись участником
+                            </h3>
+
+                            <button className='header__bottom-open'>Стать участником</button>
+                        </div>
+                       
+                    </div>
+                    <div className='giv__right'>
+                        <div className='bg'>
+
+                        </div>
+                        <div className='giv__cards'>
+                            <div className='giv__card'>
+                                <img src={i1}></img>
+                                <p className='orange'>2000</p>
+                                <span>Iphone 14 pro</span>
+                            </div>
+                            <div className='giv__card'>
+                                <img src={i2}></img>
+                                <p className='orange'>2000</p>
+                                <span>Iphone 14 pro</span>
+                            </div>
+                            <div className='giv__card'>
+                                <img src={i3}></img>
+                                <p className='orange'>2000</p>
+                                <span>Iphone 14 pro</span>
+                            </div>
+                            <div className='giv__card'>
+                                <img src={i4}></img>
+                                <p>2000</p>
+                                <span>Iphone 14 pro</span>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div className='giv__left-bottom giv__left-bottom-m'>
+                            <h3 className='giv__text'>
+                                Присоеденяйся к компании HILLHOUSE сегодня и становись участником
+                            </h3>
+
+                            <button className='header__bottom-open'>Стать участником</button>
+                        </div>
+                </div>
             </div>
-          <HeroWidget />
+          </div>
+         
+             <div className='giv__bottom'>
+                <div className='giv__bottom-wrapper container'>
+                    <h3>Результаты <span className='orange'>30</span> декабря</h3>
+                    <h3>победители будут оповещены на почту</h3>
+                </div>
+            </div>
+            <HeroWidget />
             <div className='home__middle-wrapper'>
                 <div className='home__middle container'>
                     <p className='home__middle-text'>
